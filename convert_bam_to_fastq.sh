1de71578bfd0ea11970dc5f26a50ab152c0a18fb
jenni-westoby <jenni-westoby@users.noreply.github.com>
2017-10-03 09:25:18 +0100

    Update convert_bam_to_fastq.sh

--- convert_bam_to_fastq.sh
+++ convert_bam_to_fastq.sh
@@ -12,5 +12,6 @@ do
   filename=`echo $file | awk -F/ '{print $NF}' | awk -F.bam '{print $1}'`
   $path_to_java -XX:MaxHeapSize=1000m -jar $path_to_picard_jar SamToFastq \
         I=$file \
-        FASTQ=$outdir/$filename"_1.fastq" \
+        FASTQ=$outdir/$filename".fastq" \
+       
 done
