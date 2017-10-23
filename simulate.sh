25c597a578b08fd64f4adb48f79b305da253db2f
jenni-westoby <jenni-westoby@users.noreply.github.com>
2017-10-23 10:05:50 +0100

    Update simulate.sh
    Add job control statement and frag length mean and sd

--- simulate.sh
+++ simulate.sh
@@ -18,6 +18,16 @@ run_simulations(){
     cd $memory
     #The line below will need to be edited for your LSF job system.
     bsub -n8 -R"span[hosts=1]" -c 99999 -G team_hemberg -q normal -o $TEAM/temp.logs/output.$filename -e $TEAM/temp.logs/error.$filename -R"select[mem>100000] rusage[mem=100000]" -M100000 simulate $i $1
+
+    num_jobs=`bjobs | wc -l`
+    max_jobs=100
+
+    #This prevents the number of queued jobs greatly exceeding 100.
+    while [[ $num_jobs -gt $max_jobs ]];
+    do
+      sleep 100
+      num_jobs=`bjobs | wc -l`
+    done
   done
 }
 
@@ -33,27 +43,28 @@ simulate() {
  reads="$(echo $((lines / 4)))"
 
  #Use RSEM to calculate expression
- ./Simulation/RSEM-1.3.0/rsem-calculate-expression --star\
+ ./Simulation/RSEM/rsem-calculate-expression --star\
        --star-path Simulation/STAR/bin/Linux_x86_64/ \
        -p 8 \
                    --estimate-rspd \
                    --append-names \
+                   --fragment-length-mean 1600 --fragment-length-sd 50 \
                    --output-genome-bam \
        --single-cell-prior --calc-pme \
                    $raw_data_dir/$filename \
                    Simulation/ref/reference Simulation/data/temp/$filename
+                   
+  #extract first number of third line of filename.theta, which is an estimate of the portion of reads due to background noise
+  background_noise=`sed '3q;d' Simulation/data/temp/$filename".stat"/$filename".theta" | awk '{print $1}'`
 
- #extract first number of third line of filename.theta, which is an estimate of the portion of reads due to background noise
- background_noise=`sed '3q;d' Simulation/data/temp/$filename".stat"/$filename".theta" | awk '{print $1}'`
 
+  #Simulate reads
+  ./Simulation/RSEM/rsem-simulate-reads Simulation/ref/reference Simulation/data/temp/$filename".stat"/$filename".model" \
+                                         Simulation/data/temp/$filename".isoforms.results" $background_noise $reads Simulation/data/simulated/$filename \
+                                         --seed 0
 
- #Simulate reads
- ./Simulation/RSEM-1.3.0/rsem-simulate-reads Simulation/ref/reference Simulation/data/temp/$filename".stat"/$filename".model" \
-                       Simulation/data/temp/$filename".isoforms.results" $background_noise $reads Simulation/data/simulated/$filename \
-                       --seed 0
-
- #Tidy up
- rm -r Simulation/data/temp/$filename*
+  #Tidy up
+  rm -r Simulation/data/temp/$filename*
 }
 
 export -f simulate
