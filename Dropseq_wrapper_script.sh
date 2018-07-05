83381dda890a60689a6bde6667245666f78bda42
jenni-westoby <jenniwestoby@gmail.com>
2018-07-05 17:46:11 +0100

    pipeline for testing on small data, bug fix wrapper

--- Dropseq_wrapper_script.sh
+++ Dropseq_wrapper_script.sh
@@ -19,7 +19,8 @@
 
 for i in Dropseq_Alignment_Cookbook/small_test_data/*.fastq;
 do
-  bsub -n8 -R"span[hosts=1]" -c 99999 -G team_hemberg -q normal -o $TEAM/temp.logs/output.$filename -e $TEAM/temp.logs/error.$filename -R"select[mem>100000] rusage[mem=100000]" -M100000 ./cell_level_analysis.sh $i
+  filename=`echo $i | awk -F/ '{print $3}'`
+  bsub -n8 -R"span[hosts=1]" -c 99999 -G team_hemberg -q normal -o $TEAM/temp.logs/output.$filename -e $TEAM/temp.logs/error.$filename -R"select[mem>100000] rusage[mem=100000]" -M100000 ./cell_level_analysis.sh $filename
 done
 
 # #make clean results matrices
