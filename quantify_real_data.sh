b7d03e7d8902dd12d87ac11f752904f45de8a159
jenni-westoby <jenni18@live.co.uk>
2018-07-07 16:18:32 +0100

    fix Kallisto_real bug

--- quantify_real_data.sh
+++ quantify_real_data.sh
@@ -5,6 +5,7 @@ Kallisto() {
 
       #make a directory for the results of Kallisto for each cell
       filename=$1
+      mkdir Simulation/Kallisto_real_results
       mkdir Simulation/Kallisto_real_results/$filename
 
       ./Simulation/kallisto_linux-v0.43.1/kallisto quant -i Simulation/indices/Kallisto/transcripts.idx --threads=8 --output-dir=Simulation/Kallisto_real_results/$filename --single -l 1600 -s 50 Dropseq_Alignment_Cookbook/demultiplexed_fastqs/$1'.fastq'
