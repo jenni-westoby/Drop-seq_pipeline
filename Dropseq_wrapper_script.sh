d1846216cdb511b30c27338114848767c0863e06
jenni-westoby <jenniwestoby@gmail.com>
2018-07-16 18:37:15 +0100

    Dropseq rerun

--- Dropseq_wrapper_script.sh
+++ Dropseq_wrapper_script.sh
@@ -18,31 +18,31 @@
 
 
 
-#for i in Dropseq_Alignment_Cookbook/demultiplexed_fastqs/*.fastq;
-#do
- # num_jobs=`bjobs | wc -l`
- # max_jobs=100
+for i in Dropseq_Alignment_Cookbook/demultiplexed_fastqs/*.fastq;
+do
+ num_jobs=`bjobs | wc -l`
+ max_jobs=100
 
-  #This prevents the number of queued jobs greatly exceeding 30.
-  #while [[ $num_jobs -gt $max_jobs ]];
-  #do
-   # sleep 100
-  #  num_jobs=`bjobs | wc -l`
- # done
+  This prevents the number of queued jobs greatly exceeding 30.
+  while [[ $num_jobs -gt $max_jobs ]];
+  do
+   sleep 100
+   num_jobs=`bjobs | wc -l`
+ done
 
-  #filename=`echo $i | awk -F/ '{print $3}'`
-  #bsub -n8 -R"span[hosts=1]" -c 99999 -G team_hemberg -q normal -o $TEAM/temp.logs/output.$filename -e $TEAM/temp.logs/error.$filename -R"select[mem>100000] rusage[mem=100000]" -M100000 ./cell_level_analysis.sh $filename
-#done
+  filename=`echo $i | awk -F/ '{print $3}'`
+  bsub -n8 -R"span[hosts=1]" -c 99999 -G team_hemberg -q normal -o $TEAM/temp.logs/output.$filename -e $TEAM/temp.logs/error.$filename -R"select[mem>100000] rusage[mem=100000]" -M100000 ./cell_level_analysis.sh $filename
+done
 
 # #make clean results matrices
- ./make_matrix.sh make_matrix RSEM
- ./make_matrix.sh make_matrix eXpress
- ./make_matrix.sh make_matrix Kallisto
- ./make_matrix.sh make_matrix Sailfish
- ./make_matrix.sh make_matrix Salmon
- ./make_matrix.sh make_matrix ground_truth
- ./make_matrix.sh make_matrix Kallisto_real
- ./clean_data.sh
+ # ./make_matrix.sh make_matrix RSEM
+ # ./make_matrix.sh make_matrix eXpress
+ # ./make_matrix.sh make_matrix Kallisto
+ # ./make_matrix.sh make_matrix Sailfish
+ # ./make_matrix.sh make_matrix Salmon
+ # ./make_matrix.sh make_matrix ground_truth
+ # ./make_matrix.sh make_matrix Kallisto_real
+ # ./clean_data.sh
 #
 # #move data - some of this should move into setup.sh
 # cp Simulation/results_matrices/clean* raw_results/data/
